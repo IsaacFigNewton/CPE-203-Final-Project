da052f207c553fcb4987d0fa287529a034d95656
Isaac <48895941+IsaacFigNewton@users.noreply.github.com>
2022-02-09 20:59:21 -0800

    Current UML

--- src/Active.java
+++ src/Active.java
@@ -7,26 +7,4 @@ public interface Active extends Dynamic {
     int getActionPeriod();
 
     Action createActivityAction(WorldModel world, ImageStore imageStore);
-
-//    public Action createAnimationAction(int repeatCount) {
-//        return new Action(ActionKind.ANIMATION, this, null, null,
-//                repeatCount);
-//    }
-//
-//    public Action createActivityAction(WorldModel world, ImageStore imageStore) {
-//        return new Action(ActionKind.ACTIVITY, this, world, imageStore, 0);
-//    }
-
-
-
-
-//    default void scheduleAction (EventScheduler eventScheduler, WorldModel world, ImageStore imageStore) {
-//        eventScheduler.scheduleEvent(this,
-//                this.createActivityAction(world, imageStore),
-//                this.getActionPeriod());
-//
-//        eventScheduler.scheduleEvent(this,
-//                this.createAnimationAction(0),
-//                this.getAnimationPeriod());
-//    }
 }
--- src/Plant.java
+++ src/Plant.java
@@ -2,7 +2,9 @@ import java.util.Random;
 
 public interface Plant extends Active{
     int getHealth();
+
     void decrementHealth();
+
     static int getNumFromRange(int max, int min) {
         Random rand = new Random();
         return min + rand.nextInt(max - min);
