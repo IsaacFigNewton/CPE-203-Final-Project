23b936b84815db6a49a1ebe6801c4cee7eded1db
milesfeslercp <mfesler@calpoly.edu>
2022-03-14 10:27:08 -0700

    Shrek gets removes(Isaac Changes)

--- src/Shrek.java
+++ src/Shrek.java
@@ -6,104 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 public class Shrek extends Mobile {
-//    private PathingStrategy strategy = new AStarPathingStrategy();
-//    private boolean hungie = true;
-//    private int timesEscaped = 0;
-//
-//    public Shrek(
-//            String id,
-//            Point position,
-//            List<PImage> images,
-//            int animationPeriod,
-//            int actionPeriod)
-//    {
-//        super(id, position, images, animationPeriod, actionPeriod);
-//        this.timesEscaped = 0;
-//    }
-//
-//    public Shrek(Shrek other)
-//    {
-//        super(other.getId(), other.getPosition(), other.getImages(), other.getAnimationPeriod(), other.getActionPeriod());
-//        this.timesEscaped = 0;
-//    }
-//
-//    protected void getPath(WorldModel world, Point destPos)
-//    {
-//        //recalculate path every step
-//        this.path = strategy.computePath(
-//                this.position,
-//                destPos,
-//                p -> world.withinBounds(p)
-//                        && !world.isOccupied(p),                                    // canPassThrough
-//                (p1, p2) -> p1.adjacent(p2),                                        // withinReach
-//                PathingStrategy.CARDINAL_NEIGHBORS);                                // potentialNeighbours
-//    }
-//
-//    public void executeActivity(
-//            WorldModel world,
-//            ImageStore imageStore,
-//            EventScheduler scheduler)
-//    {
-//        //if hungry
-//        //get the nearest dudeScared
-//        Optional<Entity> target =
-//                world.findNearest(this.position, new ArrayList<>(Arrays.asList(DudeScared.class)));
-//        if (this.timesEscaped <= 10){
-//            this.timesEscaped ++;
-//            DudeScared tar = (DudeScared) target.get();
-//            tar.executeActivityCondition(world, imageStore, scheduler);
-//            executeActivity(world, imageStore, scheduler);
-//        }
-//
-//        //if the ogre has consumed and is sated
-//        else if (this.hungie && target.isPresent()) {
-//            this.nextPosition(world,target.get().getPosition());
-//
-//            if(getPosition().adjacent(target.get().getPosition())){
-//                DudeScared tar = (DudeScared) target.get();
-//                tar.executeActivityCondition(world, imageStore, scheduler);
-//                executeActivity(world, imageStore, scheduler);
-//            }
-//            else{
-//                scheduler.scheduleEvent(this,
-//                        this.createActivityAction(world, imageStore),
-//                        this.actionPeriod);
-//
-//                this.hungie = false;
-//                DudeScared dudeS;
-//                if (target.isPresent()){
-//                    dudeS = (DudeScared) target.get();
-//                    dudeS.transform(world,scheduler,imageStore);
-//                }
-//
-////                ShrekFull.scheduleAction(scheduler, world, imageStore);
-//            }
-//
-//        //else if full
-//        }
-//////        if(this.timesEscaped <= 101 && target.isPresent()){
-//////            DudeScared tar = (DudeScared) target.get();
-//////            tar.executeActivityCondition(world, imageStore, scheduler);
-//////            this.timesEscaped ++;
-//////            executeActivity(world, imageStore, scheduler);
-//////
-//////        }else {
-//////            //return to swamp
-//////            Optional<Entity> newtarget =
-//////                    world.findNearest(this.position, new ArrayList<>(Arrays.asList(Swamp.class)));
-//////            System.out.println("test4");
-//////
-//////            if (newtarget.isPresent() && this.moveTo(world, newtarget.get(), scheduler)) {
-//////                world.removeEntity(this);
-//////                scheduler.unscheduleAllEvents(this);
-//////                System.out.println("test5");
-//////                ((Swamp)newtarget.get()).setHasShrek(false);
-//////                world.addEntity(newtarget.get());
-//////
-//////            }
-////        }
-//
-//    }
     private PathingStrategy strategy = new AStarPathingStrategy();
     private boolean hungie = true;
 
@@ -173,8 +75,13 @@ public class Shrek extends Mobile {
             Entity target,
             EventScheduler scheduler) {
         if (this.hungie && target.getClass() == DudeScared.class) {
+            //remove the dude scared
             world.removeEntity(target);
             scheduler.unscheduleAllEvents(target);
+
+            //remove shrek
+            world.removeEntity(this);
+            scheduler.unscheduleAllEvents(this);
 
 //            Optional<Entity> newtarget =
 //                    world.findNearest(this.position, new ArrayList<>(Arrays.asList(Swamp.class)));
